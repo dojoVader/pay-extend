345b699c816e45dd6254b3f4536ea45661f70e13
x64 <okeowoaderemi@gmail.com>
2025-09-29 17:06:48 -0400

    fix: Refactored the UI for the portal to register extensions to the platform.

--- apps/vue-dashboard/src/elements/shared/ExtensionContextAdd.vue
+++ apps/vue-dashboard/src/elements/shared/ExtensionContextAdd.vue
@@ -55,7 +55,7 @@ const onUpload = () => {
 
 <template>
     <h1 class="text-2xl font-bold pl-1 mb-5 ">Register your Extension</h1>
-    <form @submit.prevent="addExtension" class="space-y-5 p-1 mt-4 flex flex-col gap-4 text-white">
+    <form @submit.prevent="addExtension" class="space-y-5 p-1 mt-2 flex flex-col gap-4 text-white">
       <!-- Extension Item ID -->
       <div class="flex  flex-col gap-2">
         <label class="block text-xs mb-1">Extension Item ID *</label>
